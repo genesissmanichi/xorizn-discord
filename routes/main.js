a11c514ebf691ef8bb3c3bc804f8ab7c0e0df208
genesissmanichi <144709332+genesissmanichi@users.noreply.github.com>
2023-12-14 23:54:08 +0800

    Update main.js

--- routes/main.js
+++ routes/main.js
@@ -6,4 +6,7 @@ var router = express.Router();
 router.get('/', (req, res) => {
     res.redirect('https://github.com/Xorizn/xorizn-downloads#readme')
 })
+router.get('/invite', (req, res) => {
+    res.redirect('https://discord.gg/XsFYKRZTqA')
+})
 module.exports = router
